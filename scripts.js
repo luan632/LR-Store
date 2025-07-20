2ec219811a6a7f4279f36a3cf8c4a856bb696055
luan632 <holandaluan38@gmail.com>
2025-07-20 09:55:29 -0300

    site vendas online

--- scripts.js
+++ scripts.js
@@ -4,3 +4,6 @@ const items = document.querySelectorAll('.item')
 const dots = document.querySelectorAll('.dot')
 const numberIndicator = document.querySelector('numbers')
 const list = document.querySelector('.list')
+
+let active = 0;
+const total = items.length
